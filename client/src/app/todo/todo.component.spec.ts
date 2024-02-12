d26abce4f6cc30e1d82f788b934dd28dbd477150
mahon479 <mahon479@morris.umn.edu>
2024-02-12 09:13:40 -0600

    Added comments in todo component spec and service spec

--- client/src/app/todo/todo.component.spec.ts
+++ client/src/app/todo/todo.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TodoComponent } from './todo.component';
+//import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
+//import { ActivatedRoute } from '@angular/router';
+//import { TodoService } from './todo.service';
+//import { of } from 'rxjs';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
@@ -11,7 +14,7 @@ describe('TodoComponent', () => {
       imports: [TodoComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,3 +24,52 @@ describe('TodoComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+/*
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodoComponent } from './todo.component';
+import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
+import { ActivatedRoute } from '@angular/router';
+import { TodoService } from './todo.service';
+import { of } from 'rxjs';
+
+describe('TodoComponent', () => {
+  let component: TodoComponent;
+  let fixture: ComponentFixture<TodoComponent>;
+  const todoId = 'todo_id';
+  const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub({
+    id: todoId
+  });
+  const stubTodoService = {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    getTodoById: (id) => {
+      return of({
+        id: todoId,
+        owner: 'Blanche',
+        status: false,
+        body: 'In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.'
+      });
+    }
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TodoComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRoute },
+        { provide: TodoService, useValue: stubTodoService },
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TodoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+*/
--- client/src/app/todo/todo.service.spec.ts
+++ client/src/app/todo/todo.service.spec.ts
@@ -143,3 +143,42 @@ describe('TodoService', () => {
     expect(todoService).toBeTruthy();
   });
 });
+
+/*
+import { TestBed } from '@angular/core/testing';
+
+import { TodoService } from './todo.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+describe('TodoService', () => {
+  let service: TodoService;
+
+  // These are used to mock the HTTP requests so that we (a) don't have to
+  // have the server running and (b) we can check exactly which HTTP
+  // requests were made to ensure that we're making the correct requests.
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    // Set up the mock handling of the HTTP requests
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    // Construct an instance of the service with the mock
+    // HTTP client.
+    service = new TodoService(httpClient);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
+*/
